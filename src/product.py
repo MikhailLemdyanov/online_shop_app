590eef44031cacd81389dc6d7709bcefcb58ba9f
mikhail lemdyanov <lacoustique@yandex.ru>
2025-02-09 16:37:32 +0500

    add tests for category.py and product.py

--- src/product.py
+++ src/product.py
@@ -4,17 +4,9 @@ class Product:
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
 
-
-    def line(self):
-        '''Выводит строку в заданном формате'''
-        return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
-
-    def summary_price(self, other):
-        '''Складывает стоимость товаров определенной категории на складе'''
-        return (self.quantity * self.price) + (other.quantity * other.price)
