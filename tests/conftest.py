9b621fb774fbf63f6ef41c8e61633e41600431a6
mikhail lemdyanov <lacoustique@yandex.ru>
2025-02-10 20:57:16 +0500

    update conftest.py, test_category.py and test_product.py Coverage report: 98%

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,3 +17,23 @@ def product_huawei():
 @pytest.fixture
 def category_phone(product_huawei, product_iphone):
     return Category("Смартфоны", "Умные телефоны", [product_iphone, product_huawei])
+
+@pytest.fixture
+def new_product_valid_1():
+    return {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    }
+@pytest.fixture
+def new_product_valid_2():
+    return {"name": "iPhone 15", "description": "256GB, Серый цвет", "price": 210000, "quantity": 3}
+@pytest.fixture
+def new_product_valid_3():
+    return {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 3,
+    }
