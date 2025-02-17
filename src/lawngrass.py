d4e61e475065f9f15ee649f9271c696022bbd1b8
mikhail lemdyanov <lacoustique@yandex.ru>
2025-02-17 22:25:33 +0500

    add lawngrass.py

---
+++ src/lawngrass.py
@@ -0,0 +1,10 @@
+from src.product import Product
+
+
+class Lawngrass(Product):
+    'Класс, описывающий категорию товаров Трава газонная. Наследник класса Product'
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
--- src/smartphone.py
+++ src/smartphone.py
@@ -5,7 +5,7 @@ class Smartphone(Product):
     'Класс, описывающий категорию товаров Смартфон. Наследник класса Product'
     def __init__(self, name, description, price, quantity, effeciency, model, memory, color):
         super().__init__(name, description, price, quantity)
-        self.effeciency == effeciency
-        self.model == model
-        self.memory == memory
-        self.color == color
+        self.effeciency = effeciency
+        self.model = model
+        self.memory = memory
+        self.color = color
