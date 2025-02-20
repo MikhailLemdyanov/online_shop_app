4d9a4d2a062e4ddc6f27e6e4fac68d5540a68876
mikhail lemdyanov <lacoustique@yandex.ru>
2025-02-20 21:10:56 +0500

    add base_product.py and print_mixin.py

---
+++ src/print_mixin.py
@@ -0,0 +1,9 @@
+class PrintMixin:
+
+    '''Класс, который при создании объекта выводит информацию о нем'''
+
+    def __init__(self):
+        print(repr(self))
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})'
